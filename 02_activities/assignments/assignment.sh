80ffea3126e8b2833bd7bb2f599ccd6ea3e3fc67
Rupinder Hargun <rhargun@gmail.com>
2025-10-20 16:49:29 -0400

    Ficing the list issue

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -33,8 +33,7 @@ mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
 mv ./rawdata ./data/raw
 # 3. List the contents of the ./data/raw directory
-cd data/raw
-ls
+ls data/raw
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
 cd ..
 mkdir processed
