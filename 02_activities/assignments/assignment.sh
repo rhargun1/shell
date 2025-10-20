fa4e62a4237c7160d6aecb529f55c914d7fcca85
Rupinder Hargun <rhargun@gmail.com>
2025-10-20 16:54:56 -0400

    fixing cd issue for Q4

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -35,7 +35,7 @@ mv ./rawdata ./data/raw
 # 3. List the contents of the ./data/raw directory
 ls data/raw
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
-cd ..
+cd data
 mkdir processed
 cd processed
 mkdir server_logs
